da4d0e2bf6e10fac85a6b1ff0e7350df8c9ee597
nomayen <nomayen.ohin@gmail.com>
2023-01-02 01:46:21 +0600

    one

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ const users =[
 ];
 
 app.get('/users', (req, res)=>{
-    res.send(users)
+    if (req.query.name) {
+        const search =req.query.name;
+        const filtered =users.filter(usr =>usr.name.toLowerCase().indexOf(search)>=0);
+        res.send(filtered)
+    }
+    else{
+
+        res.send(users)
+    }
 })
 
 app.post('/users', (req, res)=>{
